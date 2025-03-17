cbe750973f8cf3214fb4a2bcfa511b401be69f05
Vetle444 <vetle_96@live.no>
2025-03-17 13:05:57 +0100

    done

---
+++ MauiTestApp/MauiTestApp/Bugs/Page2.xaml.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiTestApp.Bugs;
+
+public partial class Page2 : ContentPage
+{
+    public Page2()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_OnClicked(object? sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Page2());
+    }
+}
