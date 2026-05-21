2a330e346058029432d8ca2ce4c0cc6d3dba30d9
Vetle444 <vetle_96@live.no>
2026-05-21 10:05:09 +0200

    Replace with clean .NET 10 MAUI sample

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiNet10NavBarRepro;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiNet10NavBarRepro;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
